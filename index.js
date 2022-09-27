167d834b13166827c52cbd866bcb9b0543546f91
Sanal Mishra <sanamishra@BLR.ALLEGISINDIA.COM>
2022-09-27 15:00:41 +0530

    index updated

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const dotenv = require("dotenv")
 const app = express();
 dotenv.config();
-
+app.get("/", (req, res)=> {
+    res.send("Hello Bitch")
+})
+app.get("/intro" , (req,res)=>{
+    res.send("Intro Page")
+})
 const PORT  = process.env.PORT || 9000;
 app.listen(PORT,()=> {
     console.log("Server is running on " + PORT )
